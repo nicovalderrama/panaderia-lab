b24bc979305ea8e26cc37070364a1424740705bd
lautaro lobo <00loboi00@gmail.com>
2024-11-02 21:57:02 -0300

    precios

--- app/productos/models.py
+++ app/productos/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_lista = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_mayorista = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad_disponible = models.FloatField()
     unidad = models.CharField(max_length=10)
     categoria = models.CharField(max_length=50)
--- app/productos/serializer.py
+++ app/productos/serializer.py
@@ -4,6 +4,6 @@ from .models import Producto
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
-        fields = ("id", "nombre","descripcion","precio","cantidad_disponible","categoria", "imagen", "unidad")
+        fields = ("id", "nombre","descripcion","precio_lista", "precio_mayorista","cantidad_disponible","categoria", "imagen", "unidad")
         read_only_fields = ["id"]
 
